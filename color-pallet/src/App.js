5fa3ab3ccfed7d48574d4f6c99341b8a4d37d59d
Milena <milykar13@icloud.com>
2021-03-04 10:51:10 +0000

    chroma color helper added

--- color-pallet/src/App.js
+++ color-pallet/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Pallete from './pallet'
 import seedsColors from './SeedsColors';
+import {generatePalette} from './colorHelpers'
 
 
 function App() {
+  console.log(generatePalette(seedsColors[4]))
   return (
     <div className="App">
      <Pallete {...seedsColors[3]}/>
