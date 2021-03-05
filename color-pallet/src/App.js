2107dea0f5f26aba3e892143285c4e4adc366024
Milena <milykar13@icloud.com>
2021-03-05 13:00:56 +0000

    working on slider

--- color-pallet/src/App.js
+++ color-pallet/src/App.js
@@ -5,10 +5,9 @@ import {generatePalette} from './colorHelpers'
 
 
 function App() {
-  console.log(generatePalette(seedsColors[4]))
   return (
     <div className="App">
-     <Pallete {...seedsColors[3]}/>
+     <Pallete pallete={generatePalette(seedsColors[4])}/>
     </div>
   );
 }
--- color-pallet/src/pallet.js
+++ color-pallet/src/pallet.js
@@ -1,15 +1,30 @@
 import React, {Component} from 'react'
 import ColorBox from './colorBox.js'
 import './pallet.css'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 
 class Pallete extends Component {
-  render () {
+  state = {
+    level:400,
+  }
 
-    const colorBoxes = this.props.colors.map(color => (
-      <ColorBox background={color.color} name={color.name} key={color.color}/>
+  changeLevel = (level) => {
+  this.setState({level})
+  }
+
+  render () {
+    const {colors} = this.props.pallete
+    const {level} = this.state
+    const colorBoxes = colors[level].map(color => (
+      <ColorBox background={color.hex} name={color.name} key={color.color}/>
      ) )
       return (
           <div className='palette'>
+          <Slider defaultValue= {this.state.level} 
+          min={100}  max={900} 
+          step={100} 
+          onAfterChange={this.changeLevel}/>
           {/* Navbar will go here */}
           <div className='palette-colors'>{colorBoxes}</div>
           {/* Footer Will go here */}
