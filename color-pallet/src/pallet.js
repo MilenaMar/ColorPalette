755ef38c4d2cd17db45978dee25120b1556f8b69
Milena <milykar13@icloud.com>
2021-03-05 13:34:37 +0000

    navbar added

--- color-pallet/src/pallet.js
+++ color-pallet/src/pallet.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
 import ColorBox from './colorBox.js'
+import NavBar from './navBar'
 import './pallet.css'
-import Slider from 'rc-slider'
-import 'rc-slider/assets/index.css'
+
+
 
 class Pallete extends Component {
   state = {
@@ -17,15 +18,11 @@ class Pallete extends Component {
     const {colors} = this.props.pallete
     const {level} = this.state
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} key={color.color}/>
+      <ColorBox background={color.hex} name={color.name} key={color.hex}/>
      ) )
       return (
           <div className='palette'>
-          <Slider defaultValue= {this.state.level} 
-          min={100}  max={900} 
-          step={100} 
-          onAfterChange={this.changeLevel}/>
-          {/* Navbar will go here */}
+          <NavBar level={level} changeLevel={this.changeLevel}/>
           <div className='palette-colors'>{colorBoxes}</div>
           {/* Footer Will go here */}
           </div>
