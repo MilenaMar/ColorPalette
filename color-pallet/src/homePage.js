43a20ede4d48d0020cd561514c5957265fe2990a
Milena <milykar13@icloud.com>
2021-03-29 06:01:52 +0100

    styles home page started

--- color-pallet/src/homePage.js
+++ color-pallet/src/homePage.js
@@ -1,22 +1,58 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import MiniPalette from './miniPalette'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import MiniPalette from "./miniPalette";
+import { withStyles } from "@material-ui/core/styles";
 
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+      display:'flex',
+      width:'100%',
+      justifyContent:'space-between',
+      color:'white'
+  },
+  palettes: {
+      boxSizing:'border-box',
+      width:'100%',
+      display:'grid',
+      gridTemplateColumns:'repeat(3,30%)',
+      gridGap:'5%'
+  },
+};
 
 class HomePage extends Component {
-    render() {
-     const {palettes}= this.props
-        return (
-        <div>
-        <MiniPalette></MiniPalette>
+  render() {
+    const { palettes, classes } = this.props;
+    return (
+      <div className={this.props.classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
             <h1>REACT PALETTES</h1>
-            {palettes.map((palette)=>(
-                <p>
-                <MiniPalette {...palette}/>
-                </p> )) }
+          </nav>
+          <div className={classes.palettes}>
+            {palettes.map((palette) => (
+              <p>
+                <MiniPalette {...palette} />
+              </p>
+            ))}
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-export default HomePage;
+export default withStyles(styles)(HomePage);
