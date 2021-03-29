fc8aed57c468111712e8cebc3cf5b89a177a17b8
Milena <milykar13@icloud.com>
2021-03-29 05:48:41 +0100

    start styling minipalette

--- color-pallet/src/homePage.js
+++ color-pallet/src/homePage.js
@@ -12,7 +12,7 @@ class HomePage extends Component {
             <h1>REACT PALETTES</h1>
             {palettes.map((palette)=>(
                 <p>
-               <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+                <MiniPalette {...palette}/>
                 </p> )) }
         </div>
         )
